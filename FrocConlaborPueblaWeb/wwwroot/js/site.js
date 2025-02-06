a44f71ddfc2601461622082573671d079b30a10d
utp0153410@alumno.utpuebla.edu.mx <utp0153410@alumno.utpuebla.edu.mx>
2025-02-06 13:16:07 -0600

    Se agregadon los apartados a index y se ha trabajado en esta view

--- FrocConlaborPueblaWeb/wwwroot/js/site.js
+++ FrocConlaborPueblaWeb/wwwroot/js/site.js
@@ -2,3 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener('scroll', function () {
+  var contacto = document.getElementById('contacto');
+  var rect = contacto.getBoundingClientRect();
+  if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+    contacto.classList.add('full-screen-contact');
+  } else {
+    contacto.classList.remove('full-screen-contact');
+  }
+});
+
+//codigo para el encabezado
+
+// JavaScript para desplegar automáticamente el submenú cuando el cursor esté sobre "Conoce al SIUTP"
+document.addEventListener('DOMContentLoaded', function () {
+  var dropdown = document.querySelector('.nav-item.dropdown');
+
+  dropdown.addEventListener('mouseover', function () {
+    var menu = this.querySelector('.dropdown-menu');
+    var toggle = this.querySelector('.dropdown-toggle');
+    toggle.classList.add('show');
+    menu.classList.add('show');
+  });
+
+  dropdown.addEventListener('mouseout', function () {
+    var menu = this.querySelector('.dropdown-menu');
+    var toggle = this.querySelector('.dropdown-toggle');
+    toggle.classList.remove('show');
+    menu.classList.remove('show');
+  });
+});
+
+
